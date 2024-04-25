2d57809506685ca63ac3f47d47f834853eddd719
Aykut C. Satici <acsatici@gmail.com>
2024-04-25 13:58:05 -0600

    building the vanilla transformer

---
+++ vanilla/model.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, seq_len: int = 5000) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(self.seq_len, d_model)
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, self.seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply the sin function to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply the cos function to odd positions
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1) # (1, seq_len, d_model)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.size(1), :]).required_grad_(False)
+        return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps: float = 1e-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # scale (learnable)
+        self.bias = nn.Parameter(torch.zeros(1)) # shift (learnable)
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True) # don't flatten the dimension on which mean is applied
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff) # W1 and b1 (bias is set to True by default)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model) # W2 and b2
+
+    def forward(self, x):
+        # (batch_size, seq_len, d_model) -> (batch_size, seq_len, d_ff) -> (batch_size, seq_len, d_model)
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
