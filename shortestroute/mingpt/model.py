9dc4ee4d35984b32f1d3e9027f53dc8e51054d9f
Aykut C. Satici <acsatici@gmail.com>
2024-05-06 23:36:35 -0600

    it pushes through the model...

--- shortestroute/mingpt/model.py
+++ shortestroute/mingpt/model.py
@@ -50,8 +50,7 @@ class CausalSelfAttention(nn.Module):
         # causal mask to ensure that attention is only applied to the left in the input sequence
         # self.register_buffer("mask", torch.tril(torch.ones(config.block_size, config.block_size))
         #                              .view(1, 1, config.block_size, config.block_size))
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size + 1, config.block_size + 1))
-                                     .view(1, 1, config.block_size, config.block_size))
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size + 1, config.block_size + 1)).view(1, 1, config.block_size + 1, config.block_size + 1))
         self.n_head = config.n_head
         self.n_embd = config.n_embd
 
@@ -68,12 +67,12 @@ class CausalSelfAttention(nn.Module):
         att = (q @ k.transpose(-2, -1)) * ( 1.0 / math.sqrt(k.size(-1)) )
         att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
+        att = self.attn_drop(att)
         y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
 
         # output projection
-        y = self.resid_dropout(self.c_proj(y))
+        y = self.resid_drop(self.c_proj(y))
         return y
 
 class Block(nn.Module):
@@ -83,7 +82,7 @@ class Block(nn.Module):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
-        self.ln_2 = nn.LayerNarm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = nn.ModuleDict(dict(
             c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd),
             c_proj  = nn.Linear(4 * config.n_embd, config.n_embd),
@@ -125,6 +124,7 @@ class GPT(nn.Module):
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.block_size = config.block_size
+        self.config = config
 
         type_given = config.model_type is not None
         params_given = all([config.n_layer is not None, config.n_head is not None, config.n_embd is not None])
@@ -149,149 +149,181 @@ class GPT(nn.Module):
                 'gpt-nano':    dict(n_layer=3, n_head=3, n_embd=48),
             }[config.model_type])
 
-            self.transformer = nn.ModuleDict(dict(
-                te = nn.Embedding(config.vocab_size, config.n_embd),   # token embedding
-                pe = nn.Embedding(config.block_size, config.n_embd),   # position embedding
-                gpe = nn.Parameter(torch.zeros(1, config.max_timestep+1, config.n_embd)),
-                drop = nn.Dropout(config.embd_pdrop),
-                h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-                ln_f = nn.LayerNorm(config.n_embd),
-            ))
-            self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.se = nn.Linear(1, config.n_embd)   # state embedding
+        self.re = nn.Linear(1, config.n_embd)   # returns-to-go embedding
+        self.ae = nn.Embedding(config.vocab_size, config.n_embd)   # action embedding
+        self.drop = nn.Dropout(config.embd_pdrop)
+        self.blocks = nn.Sequential(*[Block(config) for _ in range(config.n_layer)])
+        self.ln_f = nn.LayerNorm(config.n_embd)
+        self.pe = nn.Parameter(torch.zeros(1, config.block_size+1, config.n_embd))
+        self.gpe = nn.Parameter(torch.zeros(1, config.max_timestep+1, config.n_embd))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-            # logger.info("number of parameters: %e", sum(p.numel() for p in self.parameters()))
+        # logger.info("number of parameters: %e", sum(p.numel() for p in self.parameters()))
 
-            # Put in state, action and reward embeddings here.
-            #
+        # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
 
-            # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
-            self.apply(self._init_weights)
-            for pn, p in self.named_parameters():
-                if pn.endswith('c_proj.weight'):
-                    nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+        # report number of parameters
+        n_params = sum(p.numel() for p in self.parameters())
+        print("number of parameters: %.2fM" % (n_params/1e6,))
 
-            # report number of parameters (note we don't count the decoder parameters in lm_head)
-            n_params = sum(p.numel() for p in self.transformer.parameters())
-            print("number of parameters: %.2fM" % (n_params/1e6,))
-
-        def _init_weights(self, module):
-            if isinstance(module, nn.Linear):
-                nn.init.normal_(module.weight, mean=0.0, std=0.02)
-                if module.bias is not None:
-                    nn.init.zeros_(module.bias)
-            elif isinstance(module, nn.Embedding):
-                nn.init.normal_(module.weight, mean=0.0, std=0.02)
-            elif isinstance(module, nn.LayerNorm):
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
                 nn.init.zeros_(module.bias)
-                nn.init.ones_(module.weight)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        elif isinstance(module, nn.LayerNorm):
+            nn.init.zeros_(module.bias)
+            nn.init.ones_(module.weight)
 
-        def get_block_size(self):
-            return self.block_size
+    def get_block_size(self):
+        return self.block_size
 
-        @classmethod
-        def from_pretrained(cls, model_type):
-            """
-            Initialize a pretrained GPT model by copying over the weights from a
-            huggingface/transformers checkpoint.
-            """
-            assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
-            from transformers import GPT2LMHeadModel
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """
+        Initialize a pretrained GPT model by copying over the weights from a
+        huggingface/transformers checkpoint.
+        """
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
 
-            # create a from-scratch initialized minGPT model
-            config = cls.get_default_config()
-            config.model_type = model_type
-            config.vocab_size = 50257 # openai's model vocabulary
-            config.block_size = 1024 # openai's model block_size
-            model = GPT(config)
-            sd = model.state_dict()
+        # create a from-scratch initialized minGPT model
+        config = cls.get_default_config()
+        config.model_type = model_type
+        config.vocab_size = 50257 # openai's model vocabulary
+        config.block_size = 1024 # openai's model block_size
+        model = GPT(config)
+        sd = model.state_dict()
 
-            # init a huggingface/transformers model
-            model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-            sd_hf = model_hf.state_dict()
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
 
-            # copy while ensuring all of the parameters are aligned and match in names and shapes
-            keys = [k for k in sd_hf if not k.endswith('attn.masked_bias')] # ignore these
-            transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
-            # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla nn.Linear.
-            # this means that we have to transpose these weights when we import them.
-            assert len(keys) == len(sd)
-            for k in keys:
-                if any(k.endswith(w) for w in transposed):
-                    # special treatment for the Conv1D weights we need to transpose
-                    assert sd_hf[k].shape[::-1] == sd[k].shape
-                    with torch.no_grad():
-                        sd[k].copy_(sd_hf[k].t())
-                else:
-                    # vanilla copy over the other parameters
-                    assert sd_hf[k].shape == sd[k].shape
-                    with torch.no_grad():
-                        sd[k].copy_(sd_hf[k])
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        keys = [k for k in sd_hf if not k.endswith('attn.masked_bias')] # ignore these
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla nn.Linear.
+        # this means that we have to transpose these weights when we import them.
+        assert len(keys) == len(sd)
+        for k in keys:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
 
-            return model
+        return model
         
-        def configure_optimizers(self, train_config):
-            """
-            This long function is unfortunately doing something very simple and is being very defensive:
-            We are separating out all parameters of the model into two buckets: those that will experience 
-            weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
-            We are then returning the PyTorch optimizer object.
-            """
+    def configure_optimizers(self, train_config):
+        """
+        This long function is unfortunately doing something very simple and is being very defensive:
+        We are separating out all parameters of the model into two buckets: those that will experience 
+        weight decay for regularization and those that won't (biases, and layernorm/embedding weights).
+        We are then returning the PyTorch optimizer object.
+        """
 
-            # separate out all parameters to those that will and won't experience regularizing weight decay
-            decay = set()
-            no_decay = set()
-            whitelist_weight_modules = (torch.nn.Linear,)
-            blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
-            for mn, m in self.named_modules():
-                for pn, p in m.named_parameters():
-                    fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
-                    # because named_modules and named_parameters are recursive we will see
-                    # the same tensors p many many times. but doing it this way allows us
-                    # to know which parent module any tensor p belongs to.
-                    if pn.endswith('bias'):
-                        # all biases will not be decayed
-                        no_decay.add(fpn)
-                    elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
-                        # weights of whitelist modules will be weight decayed
-                        decay.add(fpn)
-                    elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
-                        # weights of blacklist modules will *not* be weight decayed
-                        no_decay.add(fpn)
+        # separate out all parameters to those that will and won't experience regularizing weight decay
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear,)
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
+                # because named_modules and named_parameters are recursive we will see
+                # the same tensors p many many times. but doing it this way allows us
+                # to know which parent module any tensor p belongs to.
+                if pn.endswith('bias'):
+                    # all biases will not be decayed
+                    no_decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                    # weights of whitelist modules will be weight decayed
+                    decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                    # weights of blacklist modules will *not* be weight decayed
+                    no_decay.add(fpn)
 
-            # validate that we considered every parameter
-            param_dict = {pn: p for pn, p in self.named_parameters()}
-            inter_params = decay & no_decay
-            union_params = decay | no_decay
-            assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
-            assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
-                                                        % (str(param_dict.keys() - union_params), )
+        # validate that we considered every parameter
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                    % (str(param_dict.keys() - union_params), )
             
-            # create the pytorch optimizer object
-            optim_groups = {
-                {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
-                {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
-            }
-            optimizer = torch.optim.AdamW(optim_groups, lr=train_config.learning_rate, betas=train_config.betas)
-            return optimizer
+        # create the pytorch optimizer object
+        optim_groups = {
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        }
+        optimizer = torch.optim.AdamW(optim_groups, lr=train_config.learning_rate, betas=train_config.betas)
+        return optimizer
 
-        def forward(self, idx, targets=None):
-            device = idx.device
-            b, t = idx.size()
-            assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
-            pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
+    def forward(self, states, actions, targets=None, rtgs=None, timesteps=None):
+        # states: (batch, block_size, state_dim=1)
+        # actions: (batch, block_size, action_dim=1)
+        # targets: (batch, block_size, 1)
+        # rtgs: (batch, block_size, 1)
+        # timesteps: (batch, 1, 1)
 
-            # forward the GPT model
-            tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
-            pos_emb = self.transformer.wpe(pos) # position embeddings of shape (1, t, n_embd)
-            x = self.transformer.drop(tok_emb + pos_emb)
-            for block in self.transformer.h:
-                x = block(x)
-            x = self.transformer.ln_f(x)
-            logits = self.lm_head(x)
+        s_embedding = self.se(states) # (batch, block_size, n_embd)
 
-            # if we are given some desired targets also calculate the loss
-            loss = None
-            if targets is not None:
-                loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        if actions is not None:
+            r_embedding = self.re(rtgs.type(torch.float32))
+            a_embedding = self.ae(actions.type(torch.long).squeeze(-1))
 
-            return logits, loss
+            tok_emb = torch.zeros((states.shape[0], states.shape[1]*3 - int(targets is None), self.config.n_embd), dtype=torch.float32, device=s_embedding.device)
+            print(r_embedding.shape)
+            tok_emb[:, ::3, :] = r_embedding
+            tok_emb[:, 1::3, :] = s_embedding
+            tok_emb[:, 2::3, :] = a_embedding[:, -states.shape[1] + int(targets is None):, :]
+        elif actions is None:
+            r_embedding = self.re(rtgs.type(torch.float32))
+
+            tok_emb = torch.zeros((states.shape[0], states.shape[1]*2, self.config.n_embd), dtype=torch.float32, device=s_embedding.device)
+            tok_emb[:, ::2, :] = r_embedding
+            tok_emb[:, 1::2, :] = s_embedding
+        else:
+            raise NotImplementedError()
+
+
+        batch_size = states.shape[0]
+
+        all_global_pos_emb = torch.repeat_interleave(self.gpe, batch_size, dim=0) # (batch, block_size, n_embd)
+        pos_emb = torch.gather(
+            all_global_pos_emb,
+            1,
+            torch.repeat_interleave(timesteps, self.config.n_embd, dim=-1) 
+        ) + self.pe[:, :tok_emb.shape[1], :]
+
+        # forward the GPT model
+        x = self.drop(tok_emb + pos_emb)
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+
+        if actions is not None:
+            logits = logits[:, 1::3, :] # only keep predictions from s_embeddings
+        elif actions is None:
+            logits = logits[:, 1:, :]
+        else:
+            raise NotImplementedError()
+
+        # if we are given some desired targets also calculate the loss
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+
+        return logits, loss
